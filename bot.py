3b41cce56781975fc9a419d4cad10988a4d1c07a
Sajib <107872992+Sajib1999@users.noreply.github.com>
2025-07-17 03:05:55 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,10 @@
+import telebot
 
+TOKEN = "8097697242:AAE_Q0wWH_BNP2grnq88xF8tjmhfNNLNEXI"
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "✅ বট এখন অনলাইনে!")
+
+bot.polling()
