29cae0cba7ca498fc3582f441384ed8b3c799ad8
Sajib <107872992+Sajib1999@users.noreply.github.com>
2025-07-17 03:29:04 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import telebot
 from telebot import types
 
-TOKEN = "আপনার টোকেন বসান"
+TOKEN = "8097697242:AAE_Q0wWH_BNP2grnq88xF8tjmhfNNLNEXI"
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
